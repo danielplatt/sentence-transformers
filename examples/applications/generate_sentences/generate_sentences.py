d4b341c1ff6c9effcf44dae53e3390a4d75c96e9
Daniel Platt <Daniel.Platt@arabesque.com>
2020-07-15 16:40:31 +0100

    Generated sentences such as {“pos_sentences”: [“Companies acquired projects.“, …], “neg_sentences”: [“Companies did not acquire projects.“, …], “passive_sentences”: [“Projects were acquired by Companies.“, …], “swapped_sentences”: [“Projects acquired Companies.“, …]}

--- examples/applications/generate_sentences/generate_sentences.py
+++ examples/applications/generate_sentences/generate_sentences.py
@@ -1,9 +1,37 @@
-with open('nell_hazy_svo_604m') as f:
-    lines = []
-    for i in range(200000):
-        line = f.readline().strip().split('\t')
-        if int(line[3])>500 and line[0] != '##':
-            lines += [line]
+import lemminflect as lem
 
-print(lines)
+import json
+import codecs
 
+with codecs.open('svo_triples_lemmatised.txt', 'r', 'utf-8-sig') as json_file:
+    svo_triples = json.load(json_file)
+
+svo_triples = svo_triples[3:]
+
+pos_sentences = []
+neg_sentences = []
+passive_sentences = []
+swapped_sentences = []
+
+for triple in svo_triples:
+    triple_subject = lem.getInflection(triple[0], tag='NNS', inflect_oov=False)
+    triple_verb = lem.getInflection(triple[1], tag='VBD', inflect_oov=False)
+    triple_negative_verb = lem.getInflection(triple[1], tag='VB', inflect_oov=False)
+    triple_passive_verb = lem.getInflection(triple[1], tag='VBN', inflect_oov=False)
+    triple_object = lem.getInflection(triple[2], tag='NNS', inflect_oov=False)
+    if triple_subject != () and triple_verb != () and triple_object != ():
+        #print((triple_subject,triple_verb,triple_object))
+        #print(triple)
+        pos_sentences += [triple_subject[0].capitalize() + " " + triple_verb[0] + " " + triple_object[0] + "."]
+        neg_sentences += [triple_subject[0].capitalize() + " did not " + triple_negative_verb[0] + " " + triple_object[0] + "."]
+        passive_sentences += [triple_object[0].capitalize() + " were " + triple_passive_verb[0] + " by " + triple_subject[0] + "."]
+        swapped_sentences += [triple_object[0].capitalize() + " " + triple_verb[0] + " " + triple_subject[0] + "."]
+
+
+print(pos_sentences)
+print(neg_sentences)
+print(passive_sentences)
+print(swapped_sentences)
+
+with open('svo_generated_sentences.txt', 'w') as outfile:
+    json.dump({'pos_sentences': pos_sentences, 'neg_sentences': neg_sentences, 'passive_sentences': passive_sentences, 'swapped_sentences': swapped_sentences}, outfile)
--- examples/applications/generate_svo_triples/spacy_generate_svo.py
+++ examples/applications/generate_svo_triples/spacy_generate_svo.py
@@ -1,19 +1,7 @@
-# from nltk.corpus import wordnet as wn
-#
-# all_nouns = set()
-#
-# for synset in list(wn.all_synsets('n')):
-#     for noun in synset.lemma_names():
-#         all_nouns.add(noun.replace("_", " "))
-#
-# all_nouns_lowercase = [noun.lower() for noun in all_nouns]
 
-import enchant
-broker = enchant.Broker()
-broker.describe()
-broker.list_languages()
+from spellchecker import SpellChecker
 
-exit()
+spell = SpellChecker()
 
 pronouns = {'all','another','any','anybody','anyone','anything','as','aught','both','each','each other','either','enough','everybody','everyone','everything','few','he','her','hers','herself','him','himself','his','I','idem','it','its','itself','many','me','mine','most','my','myself','naught','neither','no one','nobody','none','nothing','nought','one','one another','other','others','ought','our','ours','ourself','ourselves','several','she','some','somebody','someone','something','somewhat','such','suchlike','that','thee','their','theirs','theirself','theirselves','them','themself','themselves','there','these','they','thine','this','those','thou','thy','thyself','us','we','what','whatever','whatnot','whatsoever','whence','where','whereby','wherefrom','wherein','whereinto','whereof','whereon','wherever','wheresoever','whereto','whereunto','wherewith','wherewithal','whether','which','whichever','whichsoever','who','whoever','whom','whomever','whomso','whomsoever','whose','whosever','whosesoever','whoso','whosoever','ye','yon','yonder','you','your','yours','yourself','yourselves'}
 
@@ -23,34 +11,48 @@ nlp = spacy.load('en_core_web_sm')
 import json
 import codecs
 
-with codecs.open('news_0056951.json', 'r', 'utf-8-sig') as json_file:
-    news_item = json.load(json_file)
-    news_item_text = news_item['text']
-    print(news_item_text)
+from pathlib import Path
+json_files = list(Path(".").rglob("*.json"))
 
-docc = nlp(news_item_text)
+print(len(json_files))
 
+#
 
+all_svo_triples = set()
+query_which_files = range(0,2000)
 
+for file_number in query_which_files:
 
-import textacy
-import re
-triples = textacy.extract.subject_verb_object_triples(docc)
+    print(file_number)
+    with codecs.open(json_files[file_number], 'r', 'utf-8-sig') as json_file:
+        news_item = json.load(json_file)
+        news_item_text = news_item['text']
+        # print(news_item_text)
 
-for trip in triples:
-    good_triple = True
-    for k in [0,1,2]:
-        # print(trip[0])
-        # print(trip[k].lemma_)
-        number_of_capital_letters = sum(1 for c in trip[k].text if c.isupper())
-        good_triple = good_triple and all(x.isalpha() for x in trip[k].text) and trip[k].text not in pronouns and number_of_capital_letters <=1 and trip[k].text != 'to' and trip[k].text != 'be'
-    if good_triple:
-        print(trip)
+    docc = nlp(news_item_text)
 
-    # print(trip)
-    # if all(x.isalpha() for x in trip[1].text) and trip[0].text.lower() in all_nouns_lowercase and trip[1].text.lower() in all_nouns_lowercase:
-    #     print(trip)
+    import textacy
+    import re
+    triples = textacy.extract.subject_verb_object_triples(docc)
 
 
+    for trip in triples:
+        good_triple = True
+        for k in [0,1,2]:
+            # print(trip[0])
+            # print(trip[k].lemma_)
+            number_of_capital_letters = sum(1 for c in trip[k].text if c.isupper())
+            good_triple = good_triple and all(x.isalpha() for x in trip[k].text) and trip[k].text not in pronouns and number_of_capital_letters <=1 and trip[k].text != 'to' and trip[k].text != 'be' and len(spell.unknown([trip[k].text]))==0 and len(trip[k].text)>4
+        if good_triple:
+            all_svo_triples.add(trip)
 
+stripspan = lambda trip: [span.lemma_ for span in trip]
 
+all_svo_triples_as_list = list(all_svo_triples)
+all_svo_triples_as_list = [stripspan(trip) for trip in all_svo_triples_as_list]
+
+print(all_svo_triples_as_list)
+print(len(all_svo_triples_as_list))
+
+with open('svo_triples_lemmatised.txt', 'w') as outfile:
+    json.dump(list(all_svo_triples_as_list), outfile)
