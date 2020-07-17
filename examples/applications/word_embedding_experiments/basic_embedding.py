e4ba16bbdb14f9fd989d9d3c90c9ab5d36b557cc
Daniel Platt <Daniel.Platt@arabesque.com>
2020-07-17 09:33:21 +0100

    Moved pca functions to evaluate_pca.py

--- examples/applications/word_embedding_experiments/basic_embedding.py
+++ examples/applications/word_embedding_experiments/basic_embedding.py
@@ -59,50 +59,6 @@ def give_difference_vectors(list_of_vectors_A, list_of_vectors_B):
     differences = [list_diff(female_vec, male_vec) for female_vec, male_vec in zip(list_of_vectors_A, list_of_vectors_B)]
     return differences
 
-def distance(subspace, linear_combination_factors, point):
-    assert(len(linear_combination_factors) == len(subspace))
-    linear_combination_factors = np.array(linear_combination_factors)
-    subspace = np.array(subspace)
-
-    # next two lines can be written as a matrix multiplication...
-    linear_combination = np.array([factor*basis_vector for factor, basis_vector in zip(linear_combination_factors, subspace)])
-    linear_combination_vector = np.sum(linear_combination, axis=0)
-
-    point = np.array(point)
-    return np.linalg.norm(linear_combination_vector - point)
-
-# print(distance([[0,1]],[100],[1,0]))
-
-def find_nearest_point_in_subspace(subspace, point):
-    function = lambda linear_factors : distance(subspace, linear_factors, point)
-    initial_guess = [0 for k in range(len(subspace))]
-    minimiser = scipy.optimize.minimize(function, initial_guess)
-
-    # next two lines can be written as a matrix multiplication...
-    subspace = np.array(subspace)
-    linear_combination = np.array([factor * basis_vector for factor, basis_vector in zip(minimiser.x, subspace)])
-    linear_combination_vector = np.sum(linear_combination, axis=0)
-
-    return linear_combination_vector
-
-print(find_nearest_point_in_subspace([[0,1]],[1,1]))
-
-def cosine_dist(x,y):
-    return 1-scipy.spatial.distance.cosine(x,y)
-
-def vector_subspace_angle(subspace, vector):
-    return cosine_dist(find_nearest_point_in_subspace(subspace,vector), vector)
-
-def vector_subspace_distance(subspace, vector):
-    return np.linalg.norm(find_nearest_point_in_subspace(subspace,vector)-vector)
-
-print("vector_subspace_angle is invariant under scaling: ")
-print(vector_subspace_angle([[0,1]],[1,1]),vector_subspace_angle([[0,1]],[5,5]))
-print("vector_subspace_distance is linear under scaling: ")
-print(vector_subspace_distance([[0,1]],[1,1]),vector_subspace_distance([[0,1]],[5,5]))
-
-exit()
-
 def store_principal_components(name, components):
     try:
         with open('principal_components.json') as json_file:
